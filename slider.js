b8468e68f1ca1f89f639e62af27eada2627fef9b
Ruslan <sasa02sasa@mail.ru>
2022-05-02 01:45:59 +0600

    refactoring code

--- slider.js
+++ slider.js
@@ -1,12 +1,20 @@
 class Slider {
-  constructor({ parentElement }) {
-    this.parentElement = parentElement;
+  constructor({ parentElement, speed = 5 }) {
+    this.elements = {
+      parentElement,
+      dots: [],
+      arrows: {
+        prevArrow: null,
+        nextArrow: null,
+      },
+      childrens: [],
+      sliderList: null,
+    };
     this.currentElement = {
       value: 1,
     };
     this.currentElementProxy = new Proxy(this.currentElement, {
       set: (target, key, value) => {
-        console.log(value);
         this.changeDots(value);
         target[key] = value;
         return true;
@@ -16,80 +24,97 @@ class Slider {
     this.sliderList = null;
     this.countElement = null;
     this.loading = false;
+    this.speed = speed;
     this.init();
-    this.cloneSlider();
-    this.addArrows();
-    this.createDots();
   }
   init = () => {
+    this.createSlider();
+    this.cloneSlider();
+    this.createArrows();
+    this.createDots();
+  };
+  createSlider = () => {
     let width = 0;
-    const childrens = this.parentElement.children;
-    let elementWidth = 0;
-    this.countElement = Array.from(childrens).length;
-    Array.from(childrens).forEach((children) => {
-      if (elementWidth == 0) elementWidth = children.clientWidth;
+
+    this.elements.childrens = Array.from(this.elements.parentElement.children);
+    this.countElement = this.elements.childrens.length;
+
+    this.elements.childrens.forEach((children) => {
+      if (this.widthElement == 0) this.widthElement = children.clientWidth;
       width += children.clientWidth;
     });
-    this.widthElement = elementWidth;
+
     width += 2 * this.widthElement;
-    let slider = `<div class="custom-slider-list" style="width:${elementWidth}px"><div class="custom-slider-track" style="width:${width}px">`;
-    Array.from(childrens).forEach((children) => {
+
+    let slider = `<div class="custom-slider-list" style="width:${this.widthElement}px"><div class="custom-slider-track" style="width:${width}px">`;
+    this.elements.childrens.forEach((children) => {
       slider += children.outerHTML;
     });
     slider += "</div></div>";
 
-    this.parentElement.innerHTML = slider;
-    this.sliderList = this.parentElement.querySelector(".custom-slider-track");
-    this.sliderList.style.transform = `translate3d(-${this.widthElement}px,0,0)`;
+    this.elements.parentElement.innerHTML = slider;
+    this.elements.sliderList = this.elements.parentElement.querySelector(".custom-slider-track");
+    this.elements.sliderList.style.transform = `translate3d(-${this.widthElement}px,0,0)`;
   };
   cloneSlider = () => {
-    const childrens = this.parentElement.querySelectorAll(".slider-element");
+    const { childrens, parentElement, sliderList } = this.elements;
     const cloneFirst = childrens[0].cloneNode(true);
     const cloneLast = childrens[childrens.length - 1].cloneNode(true);
-    this.parentElement.querySelector(".custom-slider-track").appendChild(cloneFirst);
-    this.parentElement
-      .querySelector(".custom-slider-track")
-      .insertBefore(cloneLast, this.parentElement.querySelectorAll(".slider-element")[0]);
+    sliderList.appendChild(cloneFirst);
+    sliderList.insertBefore(cloneLast, this.elements.parentElement.querySelectorAll(".slider-element")[0]);
   };
-  addArrows = () => {
-    let leftArrow = document.createElement("button");
-    leftArrow.classList.add("custom-slider-arrow", "custom-slider-arrow-left");
-    leftArrow.innerHTML = `<img src="left-arrow.png"/>`;
-    leftArrow.addEventListener("click", this.prevElement);
-    let rightArrow = document.createElement("button");
-    rightArrow.classList.add("custom-slider-arrow", "custom-slider-arrow-right");
-    rightArrow.innerHTML = `<img src="right-arrow.png"/>`;
-    rightArrow.addEventListener("click", this.nextElement);
-    this.parentElement.appendChild(leftArrow);
-    this.parentElement.appendChild(rightArrow);
+  createArrows = () => {
+    this.elements.arrows.nextArrow = this.createArrow({
+      customClass: "custom-slider-arrow-right",
+      image: "right-arrow.png",
+      handle: this.nextElement,
+    });
+    this.elements.arrows.prevArrow = this.createArrow({
+      customClass: "custom-slider-arrow-left",
+      image: "left-arrow.png",
+      handle: this.prevElement,
+    });
+  };
+  createArrow = ({ customClass, image, handle }) => {
+    const { parentElement } = this.elements;
+
+    let arrow = document.createElement("button");
+    arrow.classList.add("custom-slider-arrow", customClass);
+    arrow.innerHTML = `<img src="${image}"/>`;
+    arrow.addEventListener("click", handle);
+
+    parentElement.appendChild(arrow);
+
+    return arrow;
   };
   prevElement = () => {
     if (!this.loading) {
+      const { sliderList } = this.elements;
       requestAnimationFrame(() => {
         if (this.currentElementProxy.value == 1) {
           let start = this.currentElementProxy.value * this.widthElement;
           const int = setInterval(() => {
             this.loading = true;
-            start -= 4;
+            start -= this.speed;
             if (start == 0) {
-              this.sliderList.style.transform = `translate3d(-${this.elementWidth * this.countElement}px,0,0)`;
+              sliderList.style.transform = `translate3d(-${this.elementWidth * this.countElement}px,0,0)`;
               this.currentElementProxy.value = this.countElement;
               this.loading = false;
               clearInterval(int);
             }
-            this.sliderList.style.transform = `translate3d(-${start}px,0,0)`;
+            sliderList.style.transform = `translate3d(-${start}px,0,0)`;
           }, 1);
         } else {
           let start = this.currentElementProxy.value * this.widthElement;
           const int = setInterval(() => {
             this.loading = true;
-            start -= 4;
+            start -= this.speed;
             if (start == (this.currentElementProxy.value - 1) * this.widthElement) {
               this.currentElementProxy.value--;
               this.loading = false;
               clearInterval(int);
             }
-            this.sliderList.style.transform = `translate3d(-${start}px,0,0)`;
+            sliderList.style.transform = `translate3d(-${start}px,0,0)`;
           }, 1);
         }
       });
@@ -97,45 +122,46 @@ class Slider {
   };
   nextElement = () => {
     if (!this.loading) {
+      const { sliderList } = this.elements;
       requestAnimationFrame(() => {
         if (this.currentElementProxy.value == this.countElement) {
           let start = this.currentElementProxy.value * this.widthElement;
           const int = setInterval(() => {
             this.loading = true;
-            start += 4;
+            start += this.speed;
             if (start == (this.currentElementProxy.value + 1) * this.widthElement) {
-              this.sliderList.style.transform = `translate3d(-${this.elementWidth}px,0,0)`;
+              sliderList.style.transform = `translate3d(-${this.elementWidth}px,0,0)`;
               this.currentElementProxy.value = 1;
               this.loading = false;
               clearInterval(int);
             }
-            this.sliderList.style.transform = `translate3d(-${start}px,0,0)`;
+            sliderList.style.transform = `translate3d(-${start}px,0,0)`;
           }, 1);
         } else {
           this.currentElementProxy.value++;
           let start = (this.currentElementProxy.value - 1) * this.widthElement;
           const int = setInterval(() => {
             this.loading = true;
-            start += 4;
+            start += this.speed;
             if (start == this.currentElementProxy.value * this.widthElement) {
               this.loading = false;
               clearInterval(int);
             }
-            this.sliderList.style.transform = `translate3d(-${start}px,0,0)`;
+            sliderList.style.transform = `translate3d(-${start}px,0,0)`;
           }, 1);
         }
       });
     }
   };
   deleteActiveDot = () => {
-    let dots = document.querySelectorAll(".dot");
+    const { dots } = this.elements;
     dots.forEach((dot) => {
       if (dot.classList.contains("dot-active")) dot.classList.remove("dot-active");
     });
   };
   changeDots = (currentElement) => {
     this.deleteActiveDot();
-    let dots = document.querySelectorAll(".dot");
+    const { dots } = this.elements;
     dots.forEach((dot, index) => {
       if (index + 1 == currentElement) {
         dot.classList.add("dot-active");
@@ -147,44 +173,52 @@ class Slider {
     dots.classList.add("dots");
     for (let i = 0; i < this.countElement; i++) {
       let dot = document.createElement("button");
+      if (i == 0) dot.classList.add("dot-active");
       dot.classList.add("dot");
       dot.addEventListener("click", () => {
-        this.deleteActiveDot();
-        dot.classList.add("dot-active");
-        this.changeSlide(i + 1);
-      });
-      dots.appendChild(dot);
-    }
-    this.parentElement.appendChild(dots);
-  };
-  changeSlide = (number) => {
-    if (number != this.currentElementProxy.value) {
-      requestAnimationFrame(() => {
-        if (number > this.currentElementProxy.value) {
-          let start = this.currentElementProxy.value * this.widthElement;
-          const int = setInterval(() => {
-            start += 10;
-            if (start == number * this.widthElement) {
-              clearInterval(int);
-            }
-            this.sliderList.style.transform = `translate3d(-${start}px,0,0)`;
-          }, 1);
-          this.currentElementProxy.value = number;
-        } else {
-          let start = this.currentElementProxy.value * this.widthElement;
-          const int = setInterval(() => {
-            start -= 10;
-            if (start == number * this.widthElement) {
-              clearInterval(int);
-            }
-            this.sliderList.style.transform = `translate3d(-${start}px,0,0)`;
-          }, 1);
-          this.currentElementProxy.value = number;
+        if (!this.loading) {
+          this.deleteActiveDot();
+          dot.classList.add("dot-active");
+          this.changeSlide(i + 1);
         }
       });
+      dots.appendChild(dot);
+      this.elements.dots.push(dot);
     }
+    this.elements.parentElement.appendChild(dots);
+  };
+  changeSlide = (number) => {
+    if (number == this.currentElementProxy.value) return;
+    const { sliderList } = this.elements;
+    requestAnimationFrame(() => {
+      if (number > this.currentElementProxy.value) {
+        let start = this.currentElementProxy.value * this.widthElement;
+        const int = setInterval(() => {
+          this.loading = true;
+          start += this.speed * 2;
+          if (start == number * this.widthElement) {
+            this.loading = false;
+            clearInterval(int);
+          }
+          sliderList.style.transform = `translate3d(-${start}px,0,0)`;
+        }, 1);
+        this.currentElementProxy.value = number;
+      } else {
+        let start = this.currentElementProxy.value * this.widthElement;
+        const int = setInterval(() => {
+          this.loading = true;
+          start -= this.speed * 2;
+          if (start == number * this.widthElement) {
+            this.loading = false;
+            clearInterval(int);
+          }
+          sliderList.style.transform = `translate3d(-${start}px,0,0)`;
+        }, 1);
+        this.currentElementProxy.value = number;
+      }
+    });
   };
 }
 
 const parent = document.querySelector(".slider");
-new Slider({ parentElement: parent });
+new Slider({ parentElement: parent, speed: 10 });
